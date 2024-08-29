a5144dc69c9597885b086d609f1d0c550b2a33fb
molen5758 <molenpisang1922@gmail.com>
2024-08-29 19:37:31 +0700

    proxy

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x57f7bbcf8AF6CD92C518279F09F96be5b210FdcF'
+export const ProxyAdmin = '0xb3E345cBdBFd29Ad40CE0246A51f8A5EFfdf91A1'
+export const SWTRImplementation = '0x0A5928D1dF7dB8d386D5fB67E8EC86Ea7bc02C14'
+    
